9e2fb06c922597ce0ce1dc4926d9bc708f1d24dc
vikramC04 <vikram.chandar04@gmail.com>
2024-01-21 12:27:00 -0500

    Path Error Message (close #3)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
@@ -1,9 +1,32 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import org.apache.commons.cli.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Config {
-    public void assessValid() {
+    private static final Logger logger = LogManager.getLogger();
+    private String path;
+
+    public Config(String[] args) {
+
     }
 
-    public void configure() {
+    public void assessValidPath() throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+    }
+
+    public void configure(String[] args) throws ParseException  {
+        logger.info("** Starting Maze Runner");
+        Options options = new Options();
+        options.addOption("i", true, "Path to file containing maze");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+        this.path = cmd.getOptionValue("i");
+        logger.info("**** Reading the maze from file " + path);
     }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -16,39 +16,17 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        Config configure = new Config();
-        configure.configure();
-
-        MazeSolver solver = new MazeSolver();
-        solver.solve();
-        logger.info("** Starting Maze Runner");
-        Options options = new Options();
-        options.addOption("i", true, "Path to file containing maze");
-        CommandLineParser parser = new DefaultParser();
         try {
-            String maze_construct = "\n";
-            CommandLine cmd = parser.parse(options, args);
-            String path = cmd.getOptionValue("i");
-            logger.info("**** Reading the maze from file " + path);
-            BufferedReader reader = new BufferedReader(new FileReader(path));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        maze_construct += "WALL";
-                    } else if (line.charAt(idx) == ' ') {
-                        maze_construct += "PASS ";
-                    }
-                }
-                maze_construct += "\n";
-            }
-            logger.info(maze_construct);
+            Config conf = new Config(args);
+            conf.configure(args);
+            conf.assessValidPath();
+            MazeSolver solver = new MazeSolver();
+            solver.solve();
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
-        logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
-        logger.info("** End of MazeRunner");
-
+//        logger.info("**** Computing path");
+//        logger.info("PATH NOT COMPUTED");
+//        logger.info("** End of MazeRunner");
     }
 }
