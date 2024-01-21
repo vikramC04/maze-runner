859b7471dc3268d1504d2807160f199cfd3f9d63
vikramC04 <vikram.chandar04@gmail.com>
2024-01-21 13:18:53 -0500

    Path Sequence (close #4)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
@@ -10,23 +10,27 @@ import java.io.FileReader;
 
 public class Config {
     private static final Logger logger = LogManager.getLogger();
-    private String path;
+    private String filepath;
+    private String path_sequence;
 
     public Config(String[] args) {
 
     }
 
     public void assessValidPath() throws FileNotFoundException {
-        BufferedReader reader = new BufferedReader(new FileReader(path));
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
     }
 
     public void configure(String[] args) throws ParseException  {
         logger.info("** Starting Maze Runner");
         Options options = new Options();
         options.addOption("i", true, "Path to file containing maze");
+        options.addOption("p", true, "Path Sequence to verify");
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
-        this.path = cmd.getOptionValue("i", "./examples/small.maz.txt");
-        logger.info("**** Reading the maze from file " + path);
+        this.filepath = cmd.getOptionValue("i", "./examples/small.maz.txt");
+        this.path_sequence = cmd.getOptionValue("p");
+
+        logger.info("**** Reading the maze from file " + filepath);
     }
 }
