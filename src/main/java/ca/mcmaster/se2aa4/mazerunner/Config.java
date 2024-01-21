943dd4e75bef0acf3b2788a1e389e0dfe4dbfb5a
vikramC04 <vikram.chandar04@gmail.com>
2024-01-21 13:13:22 -0500

    -i flag accomodation and Path (close #2)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Config.java
@@ -26,7 +26,7 @@ public class Config {
         options.addOption("i", true, "Path to file containing maze");
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
-        this.path = cmd.getOptionValue("i");
+        this.path = cmd.getOptionValue("i", "./examples/small.maz.txt");
         logger.info("**** Reading the maze from file " + path);
     }
 }
