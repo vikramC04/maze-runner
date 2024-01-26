ea2e3b30166fbfdc1dc5f58e3ee8798bbca2a75d
vikramC04 <vikram.chandar04@gmail.com>
2024-01-26 14:58:43 -0500

    Solve Only (close #40)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -11,13 +11,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MazeSolver implements Solver {
-    private String path_sequence;
     private static final Logger logger = LogManager.getLogger();
     Tile west;
     Tile east;
     private final int[][] maze_binary;
-    public MazeSolver(String path_sequence, int[][] maze_m, Tile s, Tile e) throws FileNotFoundException {
-        this.path_sequence = path_sequence;
+    public MazeSolver(int[][] maze_m, Tile s, Tile e) throws FileNotFoundException {
         maze_binary = maze_m.clone();
         logger.info("Printing from maze solver: ");
         logger.info(Arrays.deepToString(maze_binary));
@@ -28,25 +26,11 @@ public class MazeSolver implements Solver {
     public void solve() {
         try {
             Processor processor = new Processor();
-            if(path_sequence != null) {
-                path_sequence = processor.processPathSequence(path_sequence);
-                Player p = new Player(west, Direction.EAST, maze_binary);
-                Player p2 = new Player(east, Direction.WEST, maze_binary);
-                boolean verdict = verifyPath(east, p)
-                        || verifyPath(west, p2);
-                if(verdict) {
-                    System.out.println("correct path");
-                } else {
-                    System.out.println("incorrect path");
-                }
-            } else {
-                logger.info("\nExecuting right hand path finding");
-                Player player = new Player(west, Direction.EAST, maze_binary);
-                String computed_path = pathFinding(east, player);
-                System.out.println("The path is: " + computed_path);
-                System.out.println("Factorized path is: " + processor.factorizePath(computed_path));
-
-            }
+            logger.info("\nExecuting right hand path finding");
+            Player player = new Player(west, Direction.EAST, maze_binary);
+            String computed_path = pathFinding(east, player);
+            System.out.println("The path is: " + computed_path);
+            System.out.println("Factorized path is: " + processor.factorizePath(computed_path));
         }catch(Exception e) {
             logger.error("error occured");
             logger.error(e.getMessage());
@@ -63,27 +47,7 @@ public class MazeSolver implements Solver {
         return path;
     }
 
-    public boolean verifyPath(Tile end, Player player) throws IOException {
-        for(char c : path_sequence.toCharArray()) {
-            if(!player.isPositionValid()) return false;
-            if(c == 'F') {
-                player.moveForward();
-            } else if(c == 'R') {
-                player.turnRight();
-            } else if(c == 'L') {
-                player.turnLeft();
-            }
-        }
-        return player.isEnd(end);
-    }
-    public String rightHandPathFinding(Tile end, Player player) {
-        String path = "";
-        while(!player.isEnd(end)) {
-            path += player.movePlayer();
-        }
 
-        return path;
-    }
 }
 
 
